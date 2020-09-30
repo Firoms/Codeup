ffd1c603f5dc37c200a34d6383bd3ba84e96ecc1
Firoms <edisonmessi@naver.com>
2020-09-30 14:40:56 +0900

    2020/09/30

---
+++ 1045.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    int h, m, s;
+    scanf("%d %d %d", &h, &m, &s);
+    float avg;
+    avg = h + m + s;
+    int sum;
+    sum = h + m + s;
+    printf("%d\n", sum);
+    printf("%0.1f", avg / 3);
+}
