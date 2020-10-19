5b9ed4e751801865c5c016fdf5706b782b6e8b06
Firoms <edisonmessi@naver.com>
2020-10-19 19:59:11 +0900

    2020/10/19

---
+++ 1056.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int a, b;
+    scanf("%d %d", &a, &b);
+    if (a == b)
+    {
+        printf("0");
+    }
+    else
+    {
+        printf("1");
+    }
+}
